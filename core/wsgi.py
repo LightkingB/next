a0b2a4aaf4ca83795bda3557e2cec5a0ad6a4d63
Nursultan Beknazarov <beknazarovnursultan@gmail.com>
2026-01-16 16:08:53 +0600

    monkey patch

--- core/wsgi.py
+++ core/wsgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 
+from decouple import config
 from django.core.wsgi import get_wsgi_application
 
+if config("DEPLOY") == 'prod':
+    from gevent import monkey
+
+    monkey.patch_all()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
