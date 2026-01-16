28037eeb499ed3ed2d9567f4da9ae1a2465a80d3
Nursultan Beknazarov <beknazarovnursultan@gmail.com>
2026-01-16 15:21:07 +0600

    pgbouncer

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,14 +1,28 @@
 from core.settings.base import *
 
 DEBUG = False
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USERNAME'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'PORT': config('PORT'),
+#         'HOST': config('HOST'),
+#         'CONN_MAX_AGE': 60,
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USERNAME'),
         'PASSWORD': config('DB_PASSWORD'),
-        'PORT': config('PORT'),
-        'HOST': config('HOST'),
-        'CONN_MAX_AGE': 60,
+        'HOST': '127.0.0.1',
+        'PORT': '6432',
+        'CONN_MAX_AGE': 0,
+        'OPTIONS': {
+            'connect_timeout': 5,
+        }
     }
 }
